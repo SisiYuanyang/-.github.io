652d3842fb811b132e0cad70b77c51a45434bbbd
zhongjialing <1229036403@qq.com>
2018-06-27 17:25:08 +0800

    love

---
+++ js/scrip.js
@@ -0,0 +1,24 @@
+/*function myFunction(x) {
+    x.classList.toggle("change");
+}*/
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
